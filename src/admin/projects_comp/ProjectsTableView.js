421337c858fd0ca189798e4439e462b4b68e3f04
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-07-04 10:32:00 +0100

    Add view for uploaded projects in table. UI inclusion for edit and delete buttons.

--- src/admin/projects_comp/ProjectsTableView.js
+++ src/admin/projects_comp/ProjectsTableView.js
@@ -14,35 +14,69 @@ class ProjectsTableView extends Component {
     render() {
         return (
             <>
-                <table className="table">
+                <table className="table table-sm">
                     <thead className="thead-dark">
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                        {
+                            this.props.projectsData.map((project) => (
+                                <tr>
+                                    <th scope="row">
+                                        <input type="checkbox"/>
+                                    </th>
+                                    <td>
+                                        <img src={project.image} style={{width: 30, height: 30}} alt={"Project IMG"}/>
+                                    </td>
+                                    <td>
+                                        <div className="card-title">{project.title}</div>
+                                    </td>
+                                    <td>
+                                        <div className="card-title">{project.description}</div>
+                                    </td>
+                                    <td>
+                                        <div className="card-title">{project.rank}</div>
+                                    </td>
+                                    <td>
+                                        <div className="card-title">
+                                            <button className="btn btn-outline-dark btn-sm">edit</button>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className="card-title">
+                                            <button className="btn btn-outline-danger btn-sm">delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
+                    {/*<tr>*/}
+                    {/*    <th scope="row">1</th>*/}
+                    {/*    <td>Mark</td>*/}
+                    {/*    <td>Otto</td>*/}
+                    {/*    <td>@mdo</td>*/}
+                    {/*</tr>*/}
+                    {/*<tr>*/}
+                    {/*    <th scope="row">2</th>*/}
+                    {/*    <td>Jacob</td>*/}
+                    {/*    <td>Thornton</td>*/}
+                    {/*    <td>@fat</td>*/}
+                    {/*</tr>*/}
+                    {/*<tr>*/}
+                    {/*    <th scope="row">3</th>*/}
+                    {/*    <td>Larry</td>*/}
+                    {/*    <td>the Bird</td>*/}
+                    {/*    <td>@twitter</td>*/}
+                    {/*</tr>*/}
                 </table>
             </>
         );
